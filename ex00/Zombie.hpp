8860d4f2ed59620f11e8f2fd0f908b308224328b
chhoflac <chhoflac@student.42.fr>
2025-02-17 10:53:43 +0100

    Zombie class implementation

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,23 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 10:36:28 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/02/17 10:36:29 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/02/17 10:52:59 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+# include <string>
+class Zombie {
+	private :
+		std::string name;
+	public :
+		void 	announce();
+		Zombie *newZombie(std::string name);
+		void	randomChump(std::string name);
+	Zombie();
+	~Zombie();
+};
+
+#endif
