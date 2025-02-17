dd32e281b3781f1422044b3abb93d736a5f592ed
chhoflac <chhoflac@student.42.fr>
2025-02-17 15:29:56 +0100

    horde done ?

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/17 15:19:04 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/02/17 15:29:25 by chhoflac         ###   ########.fr       */
+/*   Created: 2025/02/17 15:14:01 by chhoflac          #+#    #+#             */
+/*   Updated: 2025/02/17 15:14:47 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie *zombieHorde(int N, std::string name)
+int main(void)
 {
-	Zombie *horde = new Zombie[N];
-	int i;
-
-	for (i = 0; i < N; i++){
-		horde[i].setName(name);
-	}
-	return (horde);
+	
 }
