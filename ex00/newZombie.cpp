edb31803e5f5ed91729aae29b32dbd269e097f2d
chhoflac <chhoflac@student.42.fr>
2025-02-17 13:53:07 +0100

    functions implementation

--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 10:36:17 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/02/17 13:24:01 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/02/17 13:41:46 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 Zombie *newZombie(std::string name){
 	Zombie *newOne = new Zombie(name);
-	newOne->announce();
+	return (newOne);
 }
--- ex00/randomChump.cpp
+++ ex00/randomChump.cpp
@@ -6,12 +6,13 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 10:36:21 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/02/17 11:54:41 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/02/17 13:42:37 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 void	randomChump(std::string name){
-	
+	Zombie *newOne = new Zombie(name);
+	newOne->announce();
 }
