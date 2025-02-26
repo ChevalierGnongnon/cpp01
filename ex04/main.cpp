45645669ec2829928c96c8dbf327a9c19fbbeab6
chhoflac <chhoflac@student.42.fr>
2025-02-26 10:37:44 +0100

    ex04 en cours

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,12 +6,36 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 15:49:50 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/02/24 15:50:31 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/02/26 10:36:14 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
+#include <iostream>
+#include <fstream>
 
 int main(int argc, char **argv){
 	
+	std::string		line;
+	std::string		filename;
+	std::string		content;
+	std::string		s1;
+	std::string		s2;
+	std::ifstream 	file;
+	if (argc  == 4)
+	{
+		filename = argv[1];
+		s1 = argv[2];
+		s2 = argv[3];
+		file.open(filename.c_str());
+		if (!file)
+		{
+			std::cerr << "Error: could not open file" << std::endl;
+			return (0);
+		}
+		while (std::getline(file, line)){
+			std::cout << line << std::endl;
+		}
+		file.close();
+	}
 }
