dc7c29f12fab3246951f272a711793e6897bf5fd
chhoflac <chhoflac@student.42.fr>
2025-02-26 13:58:03 +0100

    ex04 a finir ...

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 15:49:50 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/02/26 10:36:14 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/02/26 13:43:16 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,35 @@ int main(int argc, char **argv){
 	std::string		content;
 	std::string		s1;
 	std::string		s2;
+	std::string		before;
 	std::ifstream 	file;
+	size_t 			pos;
+	size_t			start;
+	
 	if (argc  == 4)
 	{
 		filename = argv[1];
 		s1 = argv[2];
 		s2 = argv[3];
 		file.open(filename.c_str());
+		start = 0;
 		if (!file)
 		{
 			std::cerr << "Error: could not open file" << std::endl;
 			return (0);
 		}
 		while (std::getline(file, line)){
-			std::cout << line << std::endl;
+			content += line;
+			if (!content.empty())
+				content += "\n";
 		}
+		while (content.find(s1, pos)){
+			before = content.substr(start, )
+		}
+		std::cout << content;
 		file.close();
+	
 	}
+	else
+		std::cout << "Error: need more arguments : <filename> <s1> <s2>" << std::endl;
 }
