4111ff40eb23495b88247b55bdf185293ff9b777
chhoflac <chhoflac@student.42.fr>
2025-03-17 15:00:11 +0100

    add

--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -6,7 +6,18 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 14:05:09 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/03/17 14:05:10 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/03/17 14:34:50 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+
+class Harl{
+	private :
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+	public :
+		void	complain(std::string level);
+};
